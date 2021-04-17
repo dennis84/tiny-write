3352ec597f4b5e3c60496d44487058e27c909004
Dennis Dietrich <d.dietrich84@gmail.com>
2021-04-17 10:13:41 +0200

    link text

--- src/prosemirror/link.ts
+++ src/prosemirror/link.ts
@@ -1,6 +1,6 @@
 import {Plugin, PluginKey, TextSelection} from 'prosemirror-state'
 
-const REGEX = /(^|\s)\[([^[\]]+?)\]\((.+?)\)($|\s)/
+const REGEX = /(^|\s)\[(.+)\]\(([^ ]+)(?: "(.+)")?\)($|\s)/
 
 const findMarkPosition = (mark, doc, from, to) => {
   let markPos = {from: -1, to: -1}
@@ -64,16 +64,16 @@ const markdownLinks = (schema) => {
         return false
       }
 
-      const textFrom = $from.before()
-      const textTo = $from.after()
+      const lineFrom = $from.before()
+      const lineTo = $from.after()
 
-      const text = view.state.doc.textBetween(textFrom, textTo, '\0', '\0')
-      const match = REGEX.exec(text)
+      const line = view.state.doc.textBetween(lineFrom, lineTo, '\0', '\0')
+      const match = REGEX.exec(line)
 
       if (match) {
-        const [full,, title, href] = match
-        const spaceLeft = full.indexOf(title) - 1
-        const spaceRight = full.length - title.length - href.length - spaceLeft - 4
+        const [full,, text, href, title] = match
+        const spaceLeft = full.indexOf(text) - 1
+        const spaceRight = full.length - text.length - href.length - spaceLeft - 4
         const start = match.index + $from.start() + spaceLeft
         const end = start + full.length - spaceLeft - spaceRight
         if (sel.$from.pos >= start && sel.$from.pos <= end) {
@@ -81,13 +81,13 @@ const markdownLinks = (schema) => {
         }
 
         const textStart = start + 1
-        const textEnd = textStart + title.length
+        const textEnd = textStart + text.length
 
         if (textEnd < end) tr.delete(textEnd, end)
         if (textStart > start) tr.delete(start, textStart)
 
-        const to = start + title.length
-        tr.addMark(start, to, schema.marks.link.create({title, href}))
+        const to = start + text.length
+        tr.addMark(start, to, schema.marks.link.create({href}))
 
         const sub = end - textEnd + textStart - start
         tr.setMeta(plugin, {pos: sel.$cursor.pos - sub})
@@ -111,9 +111,10 @@ const markdownLinks = (schema) => {
     const mark = schema.marks.link.isInSet(sel.$from.marks())
 
     if (mark) {
+      const {href} = mark.attrs
       const range = findMarkPosition(mark, view.state.doc, textFrom, textTo)
-      const {title, href} = mark.attrs
-      tr.replaceRangeWith(range.from, range.to, view.state.schema.text(`[${title}](${href})`))
+      const text = view.state.doc.textBetween(range.from, range.to, '\0', '\0')
+      tr.replaceRangeWith(range.from, range.to, view.state.schema.text(`[${text}](${href})`))
       tr.setSelection(new TextSelection(tr.doc.resolve(sel.$head.pos + 1)))
       tr.setMeta(plugin, {pos: sel.$cursor.pos})
       return true
