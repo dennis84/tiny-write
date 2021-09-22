08364b6fc4abb4542c16558a593e600e9007116b
Dennis Dietrich <d.dietrich84@gmail.com>
2021-09-22 18:28:08 +0200

    table

--- src/prosemirror/extension/table.ts
+++ src/prosemirror/extension/table.ts
@@ -1,16 +1,31 @@
-import {tableNodes} from 'prosemirror-tables'
-import {Node} from 'prosemirror-model'
+import {EditorState, Selection} from 'prosemirror-state'
+import {Node, Schema, ResolvedPos} from 'prosemirror-model'
+import {InputRule, inputRules} from 'prosemirror-inputrules'
+import {keymap} from 'prosemirror-keymap'
 import {ProseMirrorExtension} from '../state'
 
+export const tableInputRule = (schema: Schema) => new InputRule(
+  new RegExp('^\\|{2,}\\s$'),
+  (state: EditorState, match: string[], start: number, end: number) => {
+    const tr = state.tr
+    const columns = [...Array(match[0].trim().length - 1)]
+    const headers = columns.map(() => schema.node(schema.nodes.table_header, {}))
+    const cells = columns.map(() => schema.node(schema.nodes.table_cell, {}))
+    const table = schema.node(schema.nodes.table, {}, [
+      schema.node(schema.nodes.table_head, {}, schema.node(schema.nodes.table_row, {}, headers)),
+      schema.node(schema.nodes.table_body, {}, schema.node(schema.nodes.table_row, {}, cells)),
+    ])
+
+    tr.delete(start, end)
+    tr.insert(start, table)
+    tr.setSelection(Selection.near(tr.doc.resolve(start + 3)))
+    return tr
+  }
+)
+
 const tableSchema = {
-  ...tableNodes({
-    tableGroup: 'block',
-    cellContent: 'inline*',
-    cellAttributes: {},
-  }),
   table: {
     content: '(table_head | table_body)*',
-    tableRole: 'table',
     isolating: true,
     group: 'block',
     parseDOM: [{tag: 'table'}],
@@ -31,6 +46,11 @@ const tableSchema = {
     selectable: false,
     parseDOM: [{tag: 'tbody'}],
     toDOM: () => ['tbody', 0],
+  },
+  table_row: {
+    content: '(table_cell | table_header)*',
+    parseDOM: [{tag: 'tr'}],
+    toDOM: () => ['tr', 0],
   },
   table_cell: {
     content: 'inline*',
@@ -64,9 +84,127 @@ const tableSchema = {
   },
 }
 
+const findParentPos = ($pos: ResolvedPos, fn: (n: Node) => boolean) => {
+  if (fn($pos.node())) return $pos
+  for (let d = $pos.depth - 1; d > 0; d--) {
+    if (fn($pos.node(d))) return $pos.node(0).resolve($pos.before(d + 1))
+  }
+  return null
+}
+
+const findTableCellPos = ($pos: ResolvedPos) =>
+  findParentPos($pos, (n) => n.type.name === 'table_cell' || n.type.name === 'table_header')
+
+const findTableRowPos = ($pos: ResolvedPos) =>
+  findParentPos($pos, (n) => n.type.name === 'table_row')
+
+const findTableHeadPos = ($pos: ResolvedPos) =>
+  findParentPos($pos, (n) => n.type.name === 'table_head')
+
+const findTablePos = ($pos: ResolvedPos) =>
+  findParentPos($pos, (n) => n.type.name === 'table')
+
+const getContentSize = (n: Node) => {
+  let size = 0
+  n.descendants((d: Node) => {
+    size += d.content.size
+  })
+
+  return size
+}
+
 export default (): ProseMirrorExtension => ({
   schema: (prev) => ({
     ...prev,
     nodes: (prev.nodes as any).append(tableSchema),
   }),
+  plugins: (prev, schema) => [
+    keymap({
+      'Ctrl-Enter': (state, dispatch) => {
+        const tablePos = findTablePos(state.selection.$head)
+        if (!tablePos) return false
+        const targetPos = tablePos.after()
+        const tr = state.tr
+        tr.insert(targetPos, state.schema.node('paragraph'))
+        tr.setSelection(Selection.near(tr.doc.resolve(targetPos)))
+        dispatch(tr)
+        return true
+      },
+      'Backspace': (state, dispatch) => {
+        const sel = state.selection
+        if (!sel.empty) return false
+        const cellPos = findTableCellPos(sel.$head)
+        if (!cellPos) return false
+
+        if (cellPos.node().content.size === 0) {
+          const rowPos = findTableRowPos(sel.$head)
+          const before = state.doc.resolve(sel.$head.pos - 2)
+          const cellBeforePos = findTableCellPos(before)
+
+          if (cellBeforePos) {
+            const tr = state.tr
+            tr.setSelection(Selection.near(before))
+            dispatch(tr)
+            return true
+          } else if (getContentSize(rowPos.node()) === 0) {
+            const tr = state.tr
+            tr.delete(before.pos - 1, before.pos + rowPos.node().nodeSize)
+            dispatch(tr)
+            return true
+          }
+        }
+
+        return false
+      },
+      'Enter': (state, dispatch) => {
+        const sel = state.selection
+        if (!sel.empty) return false
+        const cellPos = findTableCellPos(sel.$head)
+        console.log(cellPos)
+        if (!cellPos) return false
+
+        const rowPos = findTableRowPos(sel.$head)
+        const cells = []
+        rowPos.node().forEach((cell) => {
+          cells.push(schema.nodes.table_cell.create(cell.attrs))
+        })
+        const newRow = schema.nodes.table_row.create(null, cells)
+
+        const theadPos = findTableHeadPos(sel.$head)
+        if (theadPos) {
+          const tablePos = findTablePos(sel.$head)
+          let tbodyPos: number
+          tablePos.node().descendants((node, pos) => {
+            if (node.type.name === 'table_body') {
+              tbodyPos = tablePos.pos + pos
+            }
+          })
+
+          if (tbodyPos) {
+            const tbody = state.doc.resolve(tbodyPos + 1)
+            const tr = state.tr.insert(tbody.pos, newRow)
+            tr.setSelection(Selection.near(tr.doc.resolve(tbody.pos)))
+            dispatch(tr)
+          } else {
+            const tbody = schema.nodes.table_body.create(null, [newRow])
+            const targetPos = theadPos.after()
+            const tr = state.tr.insert(targetPos, tbody)
+            tr.setSelection(Selection.near(tr.doc.resolve(targetPos)))
+            dispatch(tr)
+          }
+
+          return true
+        }
+
+        const targetPos = sel.$head.after(-1)
+        const tr = state.tr.insert(targetPos, newRow)
+        tr.setSelection(Selection.near(tr.doc.resolve(targetPos)))
+
+        dispatch(tr)
+        return true
+      },
+    }),
+    ...prev,
+    inputRules({rules: [tableInputRule(schema)]}),
+  ],
 })
--- test/integration/table.test.ts
+++ test/integration/table.test.ts
@@ -1,12 +1,13 @@
-import {lineText} from './utils'
+import {clearText, lineText, move} from './utils'
 
 beforeAll(async () => {
   await page.goto('http://localhost:3000')
-  await page.click('[data-testid="burger"]')
-  await page.click('[data-testid="markdown"]')
 })
 
 it('markdown table to html and back', async () => {
+  await page.click('[data-testid="burger"]')
+  await page.click('[data-testid="markdown"]')
+
   await page.type('.ProseMirror', '| Foo | Bar | Baz |')
   await page.keyboard.press('Enter')
   await page.type('.ProseMirror', '| - | - | -: |')
@@ -37,4 +38,38 @@ it('markdown table to html and back', async () => {
   expect(await lineText(1)).toBe('| Foo | Bar | Baz |')
   expect(await lineText(2)).toBe('| --- | --- | ---:|')
   expect(await lineText(3)).toBe('| 1 | 2 | 3 |')
+
+  await page.click('[data-testid="markdown"]')
+  await page.click('[data-testid="burger"]')
+})
+
+it('table keymap', async () => {
+  await clearText()
+  await page.type('.ProseMirror', '||| ')
+  expect(await page.textContent('.ProseMirror table tr th:nth-of-type(1)')).toBe('')
+  expect(await page.textContent('.ProseMirror table tr th:nth-of-type(2)')).toBe('')
+  expect(await page.textContent('.ProseMirror table tr td:nth-of-type(1)')).toBe('')
+  expect(await page.textContent('.ProseMirror table tr td:nth-of-type(2)')).toBe('')
+  await page.type('.ProseMirror', '1')
+  await move('ArrowRight')
+  await page.type('.ProseMirror', '2')
+  await move('ArrowRight')
+  await page.type('.ProseMirror', '3')
+  await move('ArrowRight')
+  await page.keyboard.press('Backspace')
+  await page.keyboard.press('Backspace')
+  await page.keyboard.press('Backspace')
+  await page.keyboard.press('Enter')
+  await page.type('.ProseMirror', '3.1')
+  await page.keyboard.press('Enter')
+  await page.type('.ProseMirror', '5')
+  expect(await page.textContent('.ProseMirror table tr th:nth-of-type(1)')).toBe('1')
+  expect(await page.textContent('.ProseMirror table tr th:nth-of-type(2)')).toBe('2')
+  expect(await page.textContent('.ProseMirror table tr:nth-of-type(1) td:nth-of-type(1)')).toBe('3.1')
+  expect(await page.textContent('.ProseMirror table tr:nth-of-type(1) td:nth-of-type(2)')).toBe('')
+  expect(await page.textContent('.ProseMirror table tr:nth-of-type(2) td:nth-of-type(1)')).toBe('5')
+  expect(await page.textContent('.ProseMirror table tr:nth-of-type(2) td:nth-of-type(2)')).toBe('')
+  await page.keyboard.press('Control+Enter')
+  await page.type('.ProseMirror', 'outside')
+  expect(await lineText(2)).toBe('outside')
 })
