0af17d4ee2d310bbac8d6f0f778594b0872c9c64
Dennis Dietrich <d.dietrich84@gmail.com>
2021-10-19 20:22:48 +0200

    paste links

--- src/prosemirror/extension/paste-markdown.ts
+++ src/prosemirror/extension/paste-markdown.ts
@@ -1,7 +1,43 @@
 import {Plugin} from 'prosemirror-state'
-import {Schema} from 'prosemirror-model'
+import {Fragment, Node, Schema, Slice} from 'prosemirror-model'
 import {ProseMirrorExtension} from '../state'
 import {createMarkdownParser} from '../../markdown'
+
+const URL_REGEX = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/g
+
+const transform = (schema: Schema, fragment: Fragment) => {
+  const nodes = []
+  fragment.forEach((child: Node) => {
+    if (child.isText) {
+      let pos = 0
+      let match: any
+
+      while ((match = URL_REGEX.exec(child.text)) !== null) {
+        const start = match.index
+        const end = start + match[0].length
+        const attrs = {href: match[0]}
+
+        if (start > 0) {
+          nodes.push(child.cut(pos, start))
+        }
+
+        const node = child
+          .cut(start, end)
+          .mark(schema.marks.link.create(attrs).addToSet(child.marks))
+        nodes.push(node)
+        pos = end
+      }
+
+      if (pos < child.text.length) {
+        nodes.push(child.cut(pos))
+      }
+    } else {
+      nodes.push(child.copy(transform(schema, child.content)))
+    }
+  })
+
+  return Fragment.fromArray(nodes)
+}
 
 const pasteMarkdown = (schema: Schema) => {
   const parser = createMarkdownParser(schema)
@@ -19,7 +55,13 @@ const pasteMarkdown = (schema: Schema) => {
 
         const paste = parser.parse(text)
         const slice = paste.slice(0)
-        const tr = view.state.tr.replaceSelection(slice)
+        const fragment = transform(schema, slice.content)
+        const tr = view.state.tr.replaceSelection(new Slice(
+          fragment,
+          slice.openStart,
+          slice.openEnd
+        ))
+
         view.dispatch(tr)
         return true
       }
