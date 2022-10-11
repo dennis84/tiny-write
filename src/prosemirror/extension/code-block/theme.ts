21e245083af40e0703c81848ada331fd9544ecbf
Dennis Dietrich <d.dietrich84@gmail.com>
2022-10-11 12:57:36 +0200

    update dependencies

--- src/prosemirror/extension/code-block/theme.ts
+++ src/prosemirror/extension/code-block/theme.ts
@@ -1,13 +1,13 @@
 import {Extension} from '@codemirror/state'
 
-import {materialLight} from '@ddietr/codemirror-themes/theme/material-light'
-import {materialDark} from '@ddietr/codemirror-themes/theme/material-dark'
-import {solarizedLight} from '@ddietr/codemirror-themes/theme/solarized-light'
-import {solarizedDark} from '@ddietr/codemirror-themes/theme/solarized-dark'
-import {dracula} from '@ddietr/codemirror-themes/theme/dracula'
-import {githubLight} from '@ddietr/codemirror-themes/theme/github-light'
-import {githubDark} from '@ddietr/codemirror-themes/theme/github-dark'
-import {aura} from '@ddietr/codemirror-themes/theme/aura'
+import {materialLight} from '@ddietr/codemirror-themes/material-light'
+import {materialDark} from '@ddietr/codemirror-themes/material-dark'
+import {solarizedLight} from '@ddietr/codemirror-themes/solarized-light'
+import {solarizedDark} from '@ddietr/codemirror-themes/solarized-dark'
+import {dracula} from '@ddietr/codemirror-themes/dracula'
+import {githubLight} from '@ddietr/codemirror-themes/github-light'
+import {githubDark} from '@ddietr/codemirror-themes/github-dark'
+import {aura} from '@ddietr/codemirror-themes/aura'
 
 export const getTheme = (theme: string): Extension =>
   theme === 'dracula' ? dracula :
