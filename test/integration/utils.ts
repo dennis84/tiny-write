cda0bd6347a1f858067c5799d0195e62f84db258
Dennis Dietrich <d.dietrich84@gmail.com>
2022-10-12 14:40:30 +0200

    test

--- test/integration/utils.ts
+++ test/integration/utils.ts
@@ -1,19 +1,9 @@
-import {Page} from '@playwright/test'
+import {Page, expect} from '@playwright/test'
 
 export const delay = process.env.CI ? 30 : 80
 
-export const lineTextEq = async (page: Page, nth: number, text: string) =>
-  page.waitForFunction(([nth, text]) => {
-    const elem = document
-      .querySelector(`.ProseMirror > *:nth-child(${nth})`)
-      .cloneNode(true) as HTMLElement
-    elem.querySelectorAll('[contexteditable="false"]').forEach((x) => {
-      x.parentNode.removeChild(x)
-    })
-
-    const textContent = elem.textContent.replace(/\xa0/g, ' ')
-    return textContent === text
-  }, [nth, text])
+export const lineTextEq = (page: Page, nth: number, text: string) =>
+  expect(page.locator(`.ProseMirror > *:nth-child(${nth})`)).toHaveText(text)
 
 export const move = async (page: Page, key: string, repeat = 1) => {
   for (let i = 0; i < repeat; i ++) {
