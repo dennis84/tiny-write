3d73344213f21772252b96320ad20ac08ad9a4f8
Dennis Dietrich <d.dietrich84@gmail.com>
2023-06-08 13:50:52 +0200

    mod

--- test/integration/keymap.test.ts
+++ test/integration/keymap.test.ts
@@ -1,4 +1,5 @@
 import {expect, test} from '@playwright/test'
+import {mod} from '@/env'
 import {delay} from './utils'
 
 test.beforeEach(async ({page}) => {
@@ -12,9 +13,9 @@ test('on undo/redo', async ({page}) => {
   await page.type('.ProseMirror', '123', {delay})
 
   expect(await page.textContent('.ProseMirror')).toBe('text123')
-  await page.keyboard.press('Meta+z')
+  await page.keyboard.press(`${mod}+z`)
   expect(await page.textContent('.ProseMirror')).toBe('text')
-  await page.keyboard.press('Meta+y')
+  await page.keyboard.press(`${mod}+y`)
   expect(await page.textContent('.ProseMirror')).toBe('text123')
 })
 
@@ -22,7 +23,7 @@ test('on new/discard', async ({page}) => {
   await page.type('.ProseMirror', 'first', {delay})
   expect(await page.textContent('.ProseMirror')).toBe('first')
 
-  await page.keyboard.press('Meta+n')
+  await page.keyboard.press(`${mod}+n`)
   expect(await page.textContent('.ProseMirror')).toBe('Start typing ...')
 
   await page.type('.ProseMirror', 'second', {delay})
@@ -34,11 +35,11 @@ test('on new/discard', async ({page}) => {
 
   // discard
 
-  await page.keyboard.press('Meta+w')
+  await page.keyboard.press(`${mod}+w`)
   expect(await page.textContent('.ProseMirror')).toBe('Start typing ...')
   expect(await page.locator('[data-testid="file-list"] > div').count()).toBe(2)
 
-  await page.keyboard.press('Meta+w')
+  await page.keyboard.press(`${mod}+w`)
   await page.waitForTimeout(100)
   expect(await page.textContent('.ProseMirror')).toBe('first')
   expect(await page.locator('[data-testid="file-list"] > div').count()).toBe(1)
